458f26eed230bd079686928c62a82d670e7cdb35
davis4acca <davis4acca@gmail.com>
2018-05-20 16:43:46 +0100

    updated

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-var EventEmitter = require('./EventEmitter').EventEmitter;
+var EventEmitter = require("./EventEmitter").EventEmitter;
 
 var emitter = new EventEmitter();
+let eventNames = {
+  click: "click"
+};
 
-emitter.addEventListener('click', function () {
-    console.log('BUTTON PRESSED, done! ')
+// You should not rely on using magic strings, this is bad as it is easy to make a typo to cause a bug and it is hard for tools to help us find it.
+// emitter.addEventListener('click', function () {
+//     console.log('BUTTON PRESSED, done! ')
+// });
+
+// instead of 'click' create a variable which equals to 'click' if it is undefined it will be easy to debug
+emitter.addEventListener(eventNames.click, function() {
+  console.log("BUTTON PRESSED, done! ");
 });
 
-emitter.emit('click')
+emitter.emit("click");
